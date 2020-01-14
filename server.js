dd2d208ad3e5fbbe8b2aba371c433f6662974a91
Michaela DeCamara <michaela.decamara@gmail.com>
2020-01-13 20:38:19 -0500

    start project

---
+++ server.js
@@ -0,0 +1,9 @@
+// console.log('Is this my first node project?');
+const myChalk = require('chalk');
+console.log(chalk.blue('Hello, npm!'));
+require('dotenv').config();
+
+const myHttp = require('http');
+console.log(myChalk.blue('This text is blue!'));
+console.log(myChalk.greenBright('This text is green!'));
+

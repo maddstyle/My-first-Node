5a4181727c01b36f7e2073dbc4e17fbeb13edccd
maddstyle <michaela.decamara@gmail.com>
2020-09-20 09:57:51 -0400

    my first node

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-// console.log('Is this my first node project?');
+console.log("Is this my first node project?");
+
 const myChalk = require('chalk');
-console.log(chalk.blue('Hello, npm!'));
+const myHttp = require('http');
 require('dotenv').config();
 
-const myHttp = require('http');
-console.log(myChalk.blue('This text is blue!'));
-console.log(myChalk.greenBright('This text is green!'));
 
+const myServer = myHttp.createServer((request, response) => {
+    console.log(`Requested URL is: ${request.url}`);
+    if(request.url === '/') {
+        response.write('You requested localhost:3000 my friend!');
+        response.end();
+    } else if(request.url === '/ptwd102019') {
+        response.write(`
+            Kevin
+            Vero
+            Ashraf
+            Jerlissa
+        `);
+        response.end();
+    } else {
+        response.write(`You are trying to access a page that does not exist!`);
+        response.end();
+    }
+});
+
+console.log(myChalk.blue("This text is blue! "));
+console.log(myChalk.green("This text is green!"));
+
+// myServer.listen(3000, () => console.log('I am running on port 3000!'));
+
+myServer.listen(process.env.PORT, () => console.log('I am running on port 3000!'));
